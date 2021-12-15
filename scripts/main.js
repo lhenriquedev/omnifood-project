3a39f043a369ea32e364ed7a0ac9960abf756064
lhenrique.dev <lhenrique.dev@gmail.com>
2021-12-14 23:31:00 -0300

    ✨ feature: add smooth scrolling and mobile navigation

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,35 @@
 // Set current year
-const yearEl = document.querySelector('.footer-year');
+const yearEl = document.querySelector(".footer-year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 
 // =================== Mobile navigation ===================//
-const headerEl = document.querySelector('.header') 
-const btnNavEl = document.querySelector('.btn-mobile-nav')
+const headerEl = document.querySelector(".header");
+const btnNavEl = document.querySelector(".btn-mobile-nav");
 
-btnNavEl.addEventListener('click', () => {
-  headerEl.classList.toggle('nav-open')
-})
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+// ============= Smooth Scrolling animation =============== //
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") window.scrollTo({ top: 0, behavior: "smooth" });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
